bc9b2769319366e7ef8932b32e8425c9c72afc8d
Shiroyam-token <semenmaslina@gmail.com>
2022-04-16 18:43:04 +0300

    сделал верстку для Sidebar

--- app/components/layout/Sidebar/Sidebar.tsx
+++ app/components/layout/Sidebar/Sidebar.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
+import Popular from "./Popular/Popular";
+import Search from "./Search/Search";
 import s from "./sidebar.module.scss";
 
 const Sidebar: FC = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className={s.sidebar}>
+      <Search />
+      <Popular />
+    </div>
+  );
 };
 
 export default Sidebar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
+import theme from "../styles/theme/theme";
+import { ThemeProvider } from "@mui/material";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
