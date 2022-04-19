75d2bee249845831870c0dfb3cb2c549e5b754ae
Semyon Zasov <semenmaslina@gmail.com>
2022-04-19 15:38:51 +0300

    сделал верстку для Slider

--- app/components/screens/home/HomePage.tsx
+++ app/components/screens/home/HomePage.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
 import Layout from "../../layout/Layout";
+import Slider from "./Slider/Slider";
 import { IHomePage } from "./homePage.interface";
+import s from "./homePage.module.scss";
 
 const HomePage: FC<IHomePage> = () => {
   return (
-    <>
-      <Layout>
-        <p>HomePage</p>
-      </Layout>
-    </>
+    <div className={s.homePage}>
+        <div className={s.homePage__center}>
+          <Slider />
+        </div>
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
+import Layout from "../app/components/layout/Layout";
 import HomePage from "../app/components/screens/home/HomePage";
 
 const Home: NextPage = () => {
-  return <HomePage />;
+  return (<div>
+    <Layout>
+      <HomePage></HomePage>
+    </Layout>
+  </div>)
 };
 
 export default Home;
